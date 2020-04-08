61f7a4dca40fc9e28a41a8bda67496bd570d6b1c
jelarson <jesslarson93@gmail.com>
2020-04-08 13:41:28 -0600

    navlink active styles

--- src/components/app.js
+++ src/components/app.js
@@ -9,6 +9,8 @@ import PortfolioContainer from "./portfolio/portfolio-container";
 import NavigationContainer from "./navigation/navigation-container";
 import Home from "./pages/home";
 import About from "./pages/about";
+import Contact from "./pages/contact";
+import Blog from "./pages/blog";
 
 export default class App extends Component {
   render() {
@@ -22,6 +24,8 @@ export default class App extends Component {
             <Switch>
               <Route exact path='/' component={Home} />
               <Route path='/about-me' component={About} />
+              <Route path='/contact' component={Contact} />
+              <Route path='/blog' component={Blog} />
             </Switch>
           </div>
         </Router>
