0ebb909ed7c83dfa3ed1709595b2e07ade5ece11
jelarson <jesslarson93@gmail.com>
2020-04-08 13:27:42 -0600

    working with navlinks

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -1,3 +1,4 @@
+
 import React, { Component } from "react";
 
 import PortfolioItem from "./portfolio-item";
@@ -7,29 +8,50 @@ export default class PortfolioContainer extends Component {
     super();
 
     this.state = {
-      pageTitle: 'Welcome to my portfolio',
+      pageTitle: "Welcome to my portfolio",
+      isLoading: false,
       data: [
-        { title: 'Quip' },
-        { title: 'Eventbrite' },
-        { title: 'Ministry Safe' },
-        { title: 'SwingAway' }
+        { title: "Quip", category: 'eCommerce' },
+        { title: "Eventbrite", category: 'Scheduling' },
+        { title: "Ministry Safe", category: 'Enterprise' },
+        { title: "SwingAway", category: 'eCommerce' }
       ]
-    }
+    };
+
+    this.handleFilter = this.handleFilter.bind(this)
+  }
+
+  handleFilter(filter) {
+    this.setState({
+      data: this.state.data.filter(item => {
+        return item.category === filter;
+      })
+    })
   }
 
   portfolioItems() {
     return this.state.data.map(item => {
-      return <PortfolioItem title={item.title} url={'google.com'} />;
-    })
+      return <PortfolioItem title={item.title} url={"google.com"} />;
+    });
   }
 
   render() {
+    if (this.state.isLoading) {
+      return <div>Loading...</div>
+    }
+
     return (
       <div>
         <h2>{this.state.pageTitle}</h2>
 
+        <button onClick={() => this.handleFilter('eCommerce')}>eCommerce</button>
+        <button onClick={() => this.handleFilter('Scheduling')}>Scheduling</button>
+        <button onClick={() => this.handleFilter('Enterprise')}>Enterprise</button>
+
         {this.portfolioItems()}
+
+
       </div>
-    )
+    );
   }
 }
