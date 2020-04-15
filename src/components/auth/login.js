93e6217e1df8ce436624974f61b199d7218c4b27
jelarson <jesslarson93@gmail.com>
2020-04-15 15:39:47 -0600

    implementing a route guard

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -1,53 +1,89 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import axios from "axios";
 
 export default class Login extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      email: '',
-      password: ''
-    }
+      email: "",
+      password: "",
+      errorText: ""
+    };
 
-    this.handleChange = this.handleChange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
-  }
-
-  handleSubmit(event) {
-    this.setState({
-      [event.target.name]: event.target.value
-    })
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(event) {
-    console.log('handle change', event);
+    this.setState({
+      [event.target.name]: event.target.value,
+      errorText: ""
+    });
+  }
 
+  handleSubmit(event) {
+    axios
+      .post(
+        "https://api.devcamp.space/sessions",
+        {
+          client: {
+            email: this.state.email,
+            password: this.state.password
+          }
+        },
+        { withCredentials: true }
+      )
+      .then(response => {
+        if (response.data.status === "created") {
+          console.log("You can come in...");
+          this.props.handleSuccessfulAuth();
+        } else {
+          this.setState({
+            errorText: "Wrong email or password"
+          });
+          this.props.handleUnuccessfulAuth();
+        }
+      })
+      .catch(error => {
+        this.setState({
+          errorText: "An error occurred"
+        });
+        this.props.handleUnuccessfulAuth();
+      });
+
+    event.preventDefault();
   }
 
   render() {
     return (
       <div>
         <h1>LOGIN TO ACCESS YOUR DASHBOARD</h1>
+
+        <div>{this.state.errorText}</div>
+
         <form onSubmit={this.handleSubmit}>
           <input
-            type='email'
-            name='email'
-            placeholder='Your email'
-            vlaue={this.state.email}
+            type="email"
+            name="email"
+            placeholder="Your email"
+            value={this.state.email}
             onChange={this.handleChange}
           />
+
           <input
-            type='password'
-            name='password'
-            placeholder='Your password'
-            vlaue={this.state.password}
+            type="password"
+            name="password"
+            placeholder="Your password"
+            value={this.state.password}
             onChange={this.handleChange}
           />
+
           <div>
-            <button type='submit'>Login</button>
+            <button type="submit">Login</button>
           </div>
         </form>
       </div>
-    )
+    );
   }
 }
--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -1,33 +1,37 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { NavLink } from 'react-router-dom'
 
-export default class NavigationComponent extends Component {
-  constructor() {
-    super();
-  }
+const NavigationComponent = (props) => {
 
-  render() {
+  const dynamicLink = (route, linkText) => {
     return (
-      <div className='nav-wrapper'>
-        <div className='left-side'>
-          <div className='nav-link-wrapper'>
-            <NavLink exact to='/' activeClassName='nav-link-active'>Home</NavLink>
-          </div>
-          <div className='nav-link-wrapper'>
-            <NavLink to='/about-me' activeClassName='nav-link-active'>About</NavLink>
-          </div>
-          <div className='nav-link-wrapper'>
-            <NavLink to='/contact' activeClassName='nav-link-active'>Contact</NavLink>
-          </div>
-          <div className='nav-link-wrapper'>
-            <NavLink to='/blog' activeClassName='nav-link-active'>Blog</NavLink>
-          </div>
-
-        </div>
-        <div className='right-side'>
-          Jess Larson
-        </div>
-      </div >
+      <div className='nav-link-wrapper'>
+        <NavLink to='/blog' activeClassName='nav-link-active'>Blog</NavLink>
+      </div>
     )
   }
+
+  return (
+    <div className='nav-wrapper'>
+      <div className='left-side'>
+        <div className='nav-link-wrapper'>
+          <NavLink exact to='/' activeClassName='nav-link-active'>Home</NavLink>
+        </div>
+        <div className='nav-link-wrapper'>
+          <NavLink to='/about-me' activeClassName='nav-link-active'>About</NavLink>
+        </div>
+        <div className='nav-link-wrapper'>
+          <NavLink to='/contact' activeClassName='nav-link-active'>Contact</NavLink>
+        </div>
+        {props.loggedInStatus === "LOGGED_IN" ? (
+          dynamicLink("/blog", "Blog")
+        ) : null}
+      </div>
+      <div className='right-side'>
+        Jess Larson
+        </div>
+    </div >
+  )
 }
+
+export default NavigationComponent
