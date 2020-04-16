5e766540ad66fc88b18ccc191f752d8f1b6e5860
jelarson <jesslarson93@gmail.com>
2020-04-16 15:21:54 -0600

    calling api to populate sidebar list

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -1,18 +1,47 @@
 import React, { Component } from "react";
+import axios from "axios";
+
+import PortfolioSidebarList from "../portfolio/portfolio-sidebar-list";
 
 export default class PortfolioManager extends Component {
-  render() {
-    return (
-      <div className='portfolio-manager-wrapper'>
-        <div className='left-column'>
-          <h1>Portfolio Form...</h1>
-        </div>
+  constructor() {
+    super();
 
-        <div className='right-column'>
-          <h1>Portfolio sidebar...</h1>
-        </div>
-      </div>
-    )
+    this.state = {
+      portfolioItems: []
+    };
   }
 
+  getPortfolioItems() {
+    axios
+      .get("https://jesslarson93.devcamp.space/portfolio/portfolio_items", {
+        withCredentials: true
+      })
+      .then(response => {
+        this.setState({
+          portfolioItems: [...response.data.portfolio_items]
+        });
+      })
+      .catch(error => {
+        console.log("error in getPortfolioItems", error);
+      });
+  }
+
+  componentDidMount() {
+    this.getPortfolioItems();
+  }
+
+  render() {
+    return (
+      <div className="portfolio-manager-wrapper">
+        <div className="left-column">
+          <h1>Portfolio form....</h1>
+        </div>
+
+        <div className="right-column">
+          <PortfolioSidebarList data={this.state.portfolioItems} />
+        </div>
+      </div>
+    );
+  }
 }
