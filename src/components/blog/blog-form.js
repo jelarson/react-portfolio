a1b2180bafea7d3ad3b6036a6f265cd6807b50c8
jelarson <jesslarson93@gmail.com>
2020-04-24 15:18:26 -0600

    building the blog form event handlers

---
+++ src/components/blog/blog-form.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+
+export default class BlogForm extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      title: '',
+      blog_status: ''
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.props.handleSuccessfulFormSubmission(this.state);
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type='text'
+          onChange={this.handleChange}
+          name='title'
+          placeholder='Blog Title'
+          value={this.state.title}
+        />
+        <input
+          type='text'
+          onChange={this.handleChange}
+          name='blog_status'
+          placeholder='Blog Status'
+          value={this.state.blog_status}
+        />
+
+        <button>Save</button>
+      </form>
+    )
+  }
+}
--- src/components/modals/blog-modal.js
+++ src/components/modals/blog-modal.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import ReactModal from 'react-modal';
 
+import BlogForm from '../blog/blog-form'
+
 ReactModal.setAppElement('.app-wrapper')
 
 export default class BlogModal extends Component {
@@ -20,6 +22,12 @@ export default class BlogModal extends Component {
         backgroundColor: 'rgba(1, 1, 1, 0.75)'
       }
     }
+
+    this.handleSuccessfulFormSubmission = this.handleSuccessfulFormSubmission.bind(this)
+  }
+
+  handleSuccessfulFormSubmission(blog) {
+    console.log('blog from blog form', blog)
   }
 
   render() {
@@ -29,7 +37,7 @@ export default class BlogModal extends Component {
         onRequestClose={() => {
           this.props.handleModalClose();
         }} isOpen={this.props.modalIsOpen}>
-        <h1>I'm in a modal!</h1>
+        <BlogForm handleSuccessfulFormSubmission={this.handleSuccessfulFormSubmission} />
       </ReactModal>
     )
   }
