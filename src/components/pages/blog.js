777f649ee334aa104bedc5bc13b288d0ec1c8982
jelarson <jesslarson93@gmail.com>
2020-04-24 14:05:59 -0600

    refactoring infinite scroll to fix memory leak

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,9 +1,8 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import axios from 'axios'
-
-import BlogItem from '../blog/blog-item'
+import axios from "axios";
+import BlogItem from "../blog/blog-item";
 
 class Blog extends Component {
   constructor() {
@@ -14,71 +13,80 @@ class Blog extends Component {
       totalCount: 0,
       currentPage: 0,
       isLoading: true
-    }
+    };
 
-    this.getBlogItems = this.getBlogItems.bind(this)
-    this.activateInfiniteScroll();
+    this.getBlogItems = this.getBlogItems.bind(this);
+    this.onScroll = this.onScroll.bind(this);
+    window.addEventListener("scroll", this.onScroll, false);
   }
 
-  activateInfiniteScroll() {
-    window.onscroll = () => {
-      if (this.state.isLoading || this.state.blogItems.length === this.state.totalCount) {
-        return;
-      }
+  onScroll() {
+    if (
+      this.state.isLoading ||
+      this.state.blogItems.length === this.state.totalCount
+    ) {
+      return;
+    }
 
-      if (window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight) {
-        console.log('get more posts')
-      } {
-        this.getBlogItems();
-      }
+    if (
+      window.innerHeight + document.documentElement.scrollTop ===
+      document.documentElement.offsetHeight
+    ) {
+      this.getBlogItems();
     }
   }
 
   getBlogItems() {
     this.setState({
       currentPage: this.state.currentPage + 1
-    })
+    });
 
-    axios.get(`https://jesslarson93.devcamp.space/portfolio/portfolio_blogs?page=${this.state.currentPage}`,
-      { withCredentials: true })
+    axios
+      .get(
+        `https://jesslarson93.devcamp.space/portfolio/portfolio_blogs?page=${this
+          .state.currentPage}`,
+        {
+          withCredentials: true
+        }
+      )
       .then(response => {
-        console.log('getting', response.data)
+        console.log("gettting", response.data);
         this.setState({
-          blogItems: response.data.portfolio_blogs.concat(response.data.portfolio_blogs),
+          blogItems: this.state.blogItems.concat(response.data.portfolio_blogs),
           totalCount: response.data.meta.total_records,
           isLoading: false
-        })
-      }).catch(error => {
-        console.log(error)
+        });
       })
+      .catch(error => {
+        console.log("getBlogItems error", error);
+      });
   }
 
   componentWillMount() {
     this.getBlogItems();
   }
 
-  render() {
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.onScroll, false);
+  }
 
+  render() {
     const blogRecords = this.state.blogItems.map(blogItem => {
       return <BlogItem key={blogItem.id} blogItem={blogItem} />;
-    })
+    });
 
     return (
-      <div className='blog-container'>
-        <div className='content-container'>
-          {blogRecords}
-        </div>
+      <div className="blog-container">
+        <div className="content-container">{blogRecords}</div>
 
         {this.state.isLoading ? (
-
-          <div className='content-loader'>
-            <FontAwesomeIcon icon='spinner' spin />
+          <div className="content-loader">
+            <FontAwesomeIcon icon="spinner" spin />
           </div>
         ) : null}
       </div>
-    )
-
+    );
   }
 }
 
-export default Blog
+export default Blog;
