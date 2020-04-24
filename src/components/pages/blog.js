b11f7163709f93fdd44a194add7edc794a1610b6
jelarson <jesslarson93@gmail.com>
2020-04-24 16:42:41 -0600

    installing draft JS dependencies for rich text editing in react

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -105,11 +105,13 @@ class Blog extends Component {
           handleSuccessfulNewBlogSubmission={this.handleSuccessfulNewBlogSubmission}
           handleModalClose={this.handleModalClose}
           modalIsOpen={this.state.blogModalIsOpen} />
-        <div className='new-blog-link'>
-          <a onClick={this.handleNewBlogClick}>
-            Open Modal
-          </a>
-        </div>
+
+        {this.props.loggedInStatus === 'LOGGED_IN' ?
+          <div className='new-blog-link'>
+            <a onClick={this.handleNewBlogClick}>
+              <FontAwesomeIcon icon='plus-circle' />
+            </a>
+          </div> : null}
 
         <div className="content-container">{blogRecords}</div>
 
