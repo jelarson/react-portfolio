836422ee13c85ee0f0bf12c615ab28065042cbb3
jelarson <jesslarson93@gmail.com>
2020-04-24 14:53:49 -0600

    fixing screen reading warning for react modal

--- src/components/modals/blog-modal.js
+++ src/components/modals/blog-modal.js
@@ -1,16 +1,34 @@
 import React, { Component } from 'react';
 import ReactModal from 'react-modal';
 
+ReactModal.setAppElement('.app-wrapper')
+
 export default class BlogModal extends Component {
   constructor(props) {
     super(props)
+
+    this.customStyles = {
+      content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)',
+        width: '800px'
+      },
+      overlay: {
+        backgroundColor: 'rgba(1, 1, 1, 0.75)'
+      }
+    }
   }
 
   render() {
     return (
-      <ReactModal onRequestClose={() => {
-        this.props.handleModalClose();
-      }} isOpen={this.props.modalIsOpen}>
+      <ReactModal
+        style={this.customStyles}
+        onRequestClose={() => {
+          this.props.handleModalClose();
+        }} isOpen={this.props.modalIsOpen}>
         <h1>I'm in a modal!</h1>
       </ReactModal>
     )
