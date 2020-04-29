866184792f4ec56dea2853d62453e812bfd9426e
jelarson <jesslarson93@gmail.com>
2020-04-29 16:42:56 -0600

    finalizing the auth form styles

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -2,8 +2,18 @@ import React from 'react';
 
 export default function () {
   return (
-    <div>
-      About
+    <div className='content-page-wrapper'>
+      <div className='left-column'
+        style={{
+          background: 'url(https://source.unsplash.com/random) no-repeat',
+          backgroundSize: 'cover',
+          backgroundPosition: 'center'
+        }}
+      />
+      <div className='right-column'>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elementum nibh tellus molestie nunc non. Ullamcorper malesuada proin libero nunc consequat interdum. Arcu dui vivamus arcu felis. Orci phasellus egestas tellus rutrum. Enim nunc faucibus a pellentesque sit amet porttitor eget. Velit laoreet id donec ultrices tincidunt arcu. Ac odio tempor orci dapibus ultrices. Dolor sed viverra ipsum nunc. Orci phasellus egestas tellus rutrum. Tortor vitae purus faucibus ornare suspendisse sed nisi lacus sed. Dignissim cras tincidunt lobortis feugiat vivamus at.
+      </div>
+
     </div>
   )
 }
--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -1,9 +1,52 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+import loginImg from '../../../static/assets/images/auth/login.jpg'
+
 
 export default function () {
   return (
-    <div>
-      Contact
+    <div className='content-page-wrapper'>
+      <div className='left-column'
+        style={{
+          background: 'url(' + loginImg + ') no-repeat',
+          backgroundSize: 'cover',
+          backgroundPosition: 'center'
+        }}
+      />
+      <div className='right-column'>
+        <div className='contact-bullet-points'>
+          <div className='bullet-point-group'>
+            <div className='icon'>
+              <FontAwesomeIcon icon='phone' />
+            </div>
+            <div className='text'>
+              555-555-5555
+            </div>
+          </div>
+        </div>
+        <div className='contact-bullet-points'>
+          <div className='bullet-point-group'>
+            <div className='icon'>
+              <FontAwesomeIcon icon='envelope' />
+            </div>
+            <div className='text'>
+              jesse@jesse.com
+            </div>
+          </div>
+        </div>
+        <div className='contact-bullet-points'>
+          <div className='bullet-point-group'>
+            <div className='icon'>
+              <FontAwesomeIcon icon='map-marked-alt' />
+            </div>
+            <div className='text'>
+              Orem, Utah
+            </div>
+          </div>
+        </div>
+      </div>
+
     </div>
   )
 }
